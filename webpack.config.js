a283b0cc9c6630f2efef9794977fdac6412a8eee
lucagez <lucagesmundo@yahoo.it>
2019-01-02 11:26:30 +0100

    added babel transpiler for broader browser compatibility

--- webpack.config.js
+++ webpack.config.js
@@ -5,4 +5,20 @@ module.exports = {
   output: {
     path: path.join(__dirname, '/dist/'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: ['/node_modules/'],
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['env'],
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
